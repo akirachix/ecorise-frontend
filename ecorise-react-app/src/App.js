c078e54ee935a39f950aa421109374174bd68920
Eyerusalem Yrga <eyerusalemykahsay>
2025-07-20 22:52:58 +0300

    added the path

--- ecorise-react-app/src/App.js
+++ ecorise-react-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
        
-        <Route path="/navigation" element={<Navigation />} />
+        <Route path="/sidebar" element={<Navigation />} />
         
        
       </Routes>
