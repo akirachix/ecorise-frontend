7a8dbf7ecad21ca7bd0914f72db913d6a6e16e76
Eyerusalem Yrga <eyerusalemykahsay>
2025-07-21 18:02:08 +0300

    rename the teaserpage to Teaser

--- ecorise-react-app/src/App.js
+++ ecorise-react-app/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Teaser from './TeaserPage';
+import Teaser from './Teaser';
 const App = () => {
   return (
     <BrowserRouter>
