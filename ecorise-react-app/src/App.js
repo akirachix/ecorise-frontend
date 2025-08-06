4b10712c3eabb029ad19e0285f51beeeacf0b1d0
Amuor <mangaramuor@gmail.com>
2025-08-06 19:26:35 +0300

    Updated the login path

--- ecorise-react-app/src/App.js
+++ ecorise-react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="dashboard" element={<Dashboard />} />
-      <Route path="login" element={<LoginScreen/>} />
+      <Route path="/login" element={<LoginScreen/>} />
     </Routes>
   );
 }
